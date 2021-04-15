862fda2eab1957fb82cd7217a53667c3b7c57dd0
Ginés Carrascal de las Heras <gines_carrascal@es.ibm.com>
2021-04-15 13:24:38 +0200

    last changes

--- optim_wrapper.py
+++ optim_wrapper.py
@@ -4,27 +4,27 @@
 get_ipython().run_line_magic('matplotlib', 'inline')
 # Import Grover’s algorithm and components classes
 from qiskit.circuit.library import RealAmplitudes
-from qiskit.aqua.components.optimizers import COBYLA
-from qiskit.aqua.algorithms import NumPyMinimumEigensolver, VQE
-from qiskit.aqua.operators import PauliExpectation, CVaRExpectation
-from qiskit.optimization import QuadraticProgram
-from qiskit.optimization.converters import QuadraticProgramToQubo
-from qiskit.optimization.algorithms import MinimumEigenOptimizer
+from qiskit.algorithms.optimizers import COBYLA
+from qiskit.algorithms import NumPyMinimumEigensolver, VQE
+from qiskit.opflow import PauliExpectation, CVaRExpectation
+from qiskit_optimization import QuadraticProgram
+from qiskit_optimization.converters import QuadraticProgramToQubo
+from qiskit_optimization.algorithms import MinimumEigenOptimizer
 from qiskit import execute, Aer
-from qiskit.aqua import aqua_globals
+from qiskit.utils import algorithm_globals
 
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 from docplex.mp.model import Model
 
-aqua_globals.random_seed = 123456
-aqua_globals.massive=True
+algorithm_globals.random_seed = 123456
+algorithm_globals.massive=True
 
 import logging
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-# setup aqua logging
+#setup aqua logging
 from qiskit.aqua import set_qiskit_aqua_logging
 set_qiskit_aqua_logging(logging.DEBUG)  # choose INFO, DEBUG to see the log
 
@@ -41,18 +41,36 @@ optim_dict = {
   "docplex_mod": 'mdl',
   "quantum_instance": Backend,
   "shots": 1024,
-  "print":boolean,
+  "print": <boolean>,
   "solver":'method',
-  "optimizer":'SPSA',
-  "maxiter":'100',
-  "depth":'1',
-  "alpha":0.35,
+  "optimizer": SPSA ,
+  "maxiter": 100,
+  "depth": 1,
+  "alpha": 0.35,
 }
 """
 
 # Define our Optimisation function. This is the function that will be called by the quantum-aggregator class.
-def optimize_portfolio(dictionary):
+def optimize_portfolio(d):
+    #Default values
+    default_values = {
+        "quantum_instance": 'qasm_simulator',
+        "shots": 1024,
+        "print": True,
+        "solver":'vqe',
+        "entanglement": 'circular',
+        "depth":1,
+        "maxiter":1000,
+        "alpha":0.35
+        }
+
+    dictionary = {**default_values, **d}
     #dictionary["expression"]
+    if dictionary['print']:
+        print('### Parameters:')
+        print(dictionary)
+
+
     result ={}
     # case to 
     qp = QuadraticProgram()
@@ -84,12 +102,14 @@ def optimize_portfolio(dictionary):
         
         #quantum preparation
         # set classical optimizer
-    
-        optimizer = dictionary["optimizer"](maxiter=int(dictionary["maxiter"]))
+        if dictionary['print']:
+            optimizer = dictionary["optimizer"](maxiter=int(dictionary["maxiter"]), disp=True)
+        else:
+            optimizer = dictionary["optimizer"](maxiter=int(dictionary["maxiter"]))
 
         # set variational ansatz
-        var_form = RealAmplitudes(qp1.get_num_binary_vars(), reps=int(dictionary["depth"]))
-        m = var_form.num_parameters
+        ansatz = RealAmplitudes(qp1.get_num_binary_vars(), reps=int(dictionary["depth"]), entanglement=dictionary["entanglement"])
+        m = ansatz.num_parameters
 
         # set backend
         backend = Aer.get_backend(dictionary["quantum_instance"])
@@ -98,8 +118,28 @@ def optimize_portfolio(dictionary):
         cvar_exp = CVaRExpectation(float(dictionary["alpha"]), PauliExpectation())
         cvar_exp.compute_variance = lambda x: [0]  # to be fixed in PR #1373
 
+        counts = []
+        values = []
+        def store_intermediate_result(eval_count, parameters, mean, std):
+            counts.append(eval_count)
+            values.append(mean)
+            if dictionary['print']:
+                print('   Iteration:',eval_count,'    Value:', mean)
+
+
         # initialize VQE using CVaR
-        vqe = VQE(expectation=cvar_exp, optimizer=optimizer, var_form=var_form, quantum_instance=backend)
+        if ("initial_point" in dictionary):
+            vqe = VQE(expectation=cvar_exp, optimizer=optimizer, ansatz=ansatz, quantum_instance=backend,
+                        #callback=store_intermediate_result,
+                        initial_point=dictionary["initial_point"]   
+                    )
+        else:
+            vqe = VQE(expectation=cvar_exp, optimizer=optimizer, ansatz=ansatz, quantum_instance=backend,
+                        #callback=store_intermediate_result
+                    )
+
+        if dictionary['print']:
+            print(vqe.print_settings())
 
         # initialize optimization algorithm based on CVaR-VQE
         opt_alg = MinimumEigenOptimizer(vqe)
@@ -110,10 +150,13 @@ def optimize_portfolio(dictionary):
         t_0 = time.perf_counter() - t_00
         result['computational_time'] = t_0
         result['result'] = conv.interpret(results) # convert a result of a converted problem into that of the original problem.
+        result['counts'] = counts
+        result['values'] = values
 
         # print results
     if dictionary['print']:
         print('### Results:')
-        print(result)
+        print('Time:',result['computational_time']/60,'minutes')
+        print('Result:', result['result'])
         
     return result
